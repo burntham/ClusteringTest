ba7f18eff5a5e14cd2d330be09fbfa92107d749c
Daniel Burnham-King <drbking@gmail.com>
2014-09-09 11:37:10 +0200

    kPow

--- clusterT/src/clusterT.cpp
+++ clusterT/src/clusterT.cpp
@@ -1,13 +1,27 @@
 //============================================================================
 // Name        : clusterT.cpp
-// Author      : 
+// Author      :
 // Version     :
 // Copyright   : TBA
 // Description : Hello World in C++, Ansi-style
 //============================================================================
 
 #include <iostream>
+#include <vcg/simplex/face/base.h>
+//#include <vcg/simplex/vertex/base.h>
+
 using namespace std;
+using namespace vcg;
+//class MyVertex;
+//class MyEdge;
+//class MyFace;
+//
+//class MyUsedTypes: public UsedTypes<Use<MyVertex>::AsVertexType,
+//		Use<MyEdge>::AsEdgeType, Use<MyFace>::AsFaceType> {
+//};
+//
+//class MyVertex: public Vertex<MyUsedTypes, vertex::VFAdj, vertex::Coord3f,
+//		vertex::Normal3f, vertex::Mark, vertex::BitFlags> {};
 
 int main() {
 	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
